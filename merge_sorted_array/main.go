4848eb605656c1fb927f0be23429e6d16ec22f15
Kuatbek Kazhytai <kuatbekkazhytai@Kuatbeks-MacBook-Air.local>
2023-04-20 13:31:57 +0900

    code clearence

--- merge_sorted_array/main.go
+++ merge_sorted_array/main.go
@@ -10,6 +10,7 @@ func main() {
 	merge(nums1, m, nums2, n)
 	fmt.Println(nums1)
 }
+
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	index1 := 0
 	index2 := 0
@@ -41,43 +42,3 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 	copy(nums1, cp)
 }
-
-//func merge(nums1 []int, m int, nums2 []int, n int) {
-//	index1 := 0
-//	index2 := 0
-//	cp := make([]int, len(nums1))
-//	copy(cp, nums1)
-//
-//	for i := 0; i < m+n; i++ {
-//		if index2 == len(nums2) {
-//			break
-//		}
-//		if nums1[index1] <= nums2[index2] && nums1[index1] != 0 {
-//			if i == len(nums1) {
-//				nums1 = append(nums1, nums1[index1])
-//			} else {
-//				nums1[i] = nums1[index1]
-//			}
-//
-//			index1++
-//		} else {
-//			if nums1[index1] == 0 && i < m && nums2[index2] > 0 {
-//				if i == len(nums1) {
-//					nums1 = append(nums1, nums1[index1])
-//				} else {
-//					nums1[i] = nums1[index1]
-//				}
-//				index1++
-//				continue
-//			}
-//
-//			x := nums1[i:]
-//			x = x[:len(x)-1]
-//			temp := append([]int{nums2[index2]}, x...)
-//			nums1 = append(nums1[:i], temp...)
-//			index1++
-//			index2++
-//		}
-//	}
-//	fmt.Println(nums1)
-//}
