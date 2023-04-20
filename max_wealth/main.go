09b94681e961312ca2f186b4cc881d4b5f621227
Kuatbek Kazhytai <kuatbekkazhytai@Kuatbeks-MacBook-Air.local>
2023-04-20 19:27:37 +0900

    1672

---
+++ max_wealth/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := [][]int{{1, 2, 3}, {3, 2, 1}}
+	fmt.Println(maximumWealth(nums))
+}
+
+func maximumWealth(accounts [][]int) int {
+	max := 0
+	for i := 0; i < len(accounts); i++ {
+		if count(accounts[i]) > max {
+			max = count(accounts[i])
+		}
+	}
+
+	return max
+}
+
+func count(nums []int) int {
+	sum := 0
+
+	for _, j := range nums {
+		sum += j
+	}
+
+	return sum;
+}
